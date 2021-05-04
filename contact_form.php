5eac110c1172eb61cc69edb26913fb9a3ad771c5
Yucas <63893751+yukayuca@users.noreply.github.com>
2021-05-04 23:57:44 +0900

    Create contact_form.php

---
+++ contact_form.php
@@ -0,0 +1,26 @@
+<?php 
+    session_start(); 
+    $mode = 'input'; 
+    $errmessage = array(); 
+    if( isset($_POST['back']) && $_POST['back'] ){
+
+    } else if( isset($_POST)['confirm']) && $_POST['confirm'] ){
+        //確認画面
+        if()
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>お問い合わせフォーム</title>
+</head>
+<body>
+
+
+</body>
+</html>
+
